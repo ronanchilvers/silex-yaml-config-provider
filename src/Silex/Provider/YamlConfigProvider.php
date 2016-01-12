c1464e9f9d0802c56414847eb43868a7eaab99d8
Ronan Chilvers <ronan@d3r.com>
2016-01-12 21:45:34 +0000

    First draft version

---
+++ src/Silex/Provider/YamlConfigProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Ronanchilvers\Silex\Provider;
+
+use Silex\ServiceProviderInterface;
+use Symfony\Component\Yaml\Parser as YamlParser;
+
+/**
+ * Simple class to provide a config service parsed from a YAML file
+ * 
+ * @author Ronan Chilvers <ronan@d3r.com>
+ */
+class YamlConfigProvider implements ServiceProviderInterface
+{
+    /**
+     * Register this provider
+     *
+     * @param Silex\Application $app
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function register(Application $app)
+    {
+        $app['config.path'] = null;
+
+        $app['config'] = $app->share(function(Application $app) {
+            if (is_null($app['config.path'])) {
+                throw new \RuntimeException('You must provide a valid config path in config.path');
+            }
+            if (!file_exists($app['config.path'])) {
+                throw new \RuntimeException(sprintf('Config path \'%s\' is not valid', $app['config.path']));
+            }
+            if (!is_readable($app['config.path'])) {
+                throw new \RuntimeException(sprintf('Config path \'%s\' is not readable', $app['config.path']));
+            }
+            $parser = new YamlParser();
+            $config = $parser->parse(file_get_contents($app['config.path']));
+
+            return $config;
+        });
+    }
+
+    /**
+     * Boot the provider.
+     *
+     * @param Silex\Application $app
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function boot(Application $app)
+    {
+    }
+
+}
